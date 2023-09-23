3a39fb8ce951a26cfb43c44b8dea4b38dd66058e
ubungmeister <ubungmeister@gmail.com>
2023-09-23 20:47:48 +0200

    syling

--- project/my-app/src/compnents/pocket-money/PmControls.tsx
+++ project/my-app/src/compnents/pocket-money/PmControls.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import { BsArrowLeftSquare, BsArrowRightSquare, BsSave } from 'react-icons/bs'
+import { TfiBackLeft } from 'react-icons/tfi'
 import { PmControlsProps } from './types'
 
 const PmControls = ({
@@ -8,15 +9,37 @@ const PmControls = ({
   setChangeCancel,
 }: PmControlsProps) => {
   return (
-    <div>
-      <div className="flex flex-row justify-between">
-        <div className="flex space-x-10">
-          <button onClick={() => setIsMonthChange('prev')}>Previous</button>
-          <button onClick={() => setIsMonthChange('next')}>Next</button>
+    <div className="">
+      <div className="flex flex-row justify-start">
+        <div className="flex space-x-3 mx-5">
+          <button onClick={() => setIsMonthChange('prev')}>
+            <BsArrowLeftSquare className="icons-controls" />
+          </button>
+          <button onClick={() => setIsMonthChange('next')}>
+            <BsArrowRightSquare className="icons-controls" />
+          </button>
         </div>
-        <div className="flex space-x-10 px-20">
-          <button onClick={() => handleSavePm()}>Save</button>
-          <button onClick={() => setChangeCancel(true)}>Cancel</button>
+        <div className=" border-l border-gray-300 h-8"></div>
+        <div className="flex space-x-3 px-10">
+          <button className="button-month px-5" onClick={() => handleSavePm()}>
+            <div className="flex flex-row space-x-2">
+              <div className="py-1">
+                <BsSave />
+              </div>
+              <span>Save</span>
+            </div>
+          </button>
+          <button
+            className="button-empty px-5"
+            onClick={() => setChangeCancel(true)}
+          >
+            <div className="flex flex-row space-x-2">
+              <div className="py-1">
+                <TfiBackLeft />
+              </div>
+              <span>Cancel</span>
+            </div>
+          </button>
         </div>
       </div>
     </div>
--- project/my-app/src/compnents/pocket-money/PmTable.tsx
+++ project/my-app/src/compnents/pocket-money/PmTable.tsx
@@ -20,28 +20,35 @@ const PmTable = ({
         const amount = pocketMoneyEntry ? pocketMoneyEntry.amount : 0
 
         return (
-          <input
-            key={index}
-            value={amount}
-            type="number"
-            min={0}
-            max={1000000}
-            onChange={e =>
-              handleInputChange(
-                e.target.value,
-                monthEntry.month,
-                user.id,
-                pocketMoneyEntry?.id
-              )
-            }
-          />
+          <div className=" min-w-[8rem] max-w-[8rem] text-center">
+            <input
+              key={index}
+              value={amount}
+              type="number"
+              min={0}
+              max={1000000}
+              className=" min-w-[5rem] max-w-[5rem] border-2 border-gray-300 rounded-md p-1"
+              onChange={e =>
+                handleInputChange(
+                  e.target.value,
+                  monthEntry.month,
+                  user.id,
+                  pocketMoneyEntry?.id
+                )
+              }
+            />
+          </div>
         )
       })
 
       return (
-        <div className="flex  flex-row justify-between pr-60">
-          <h3 className="px-5 max-w-[20%] overflow-hidden ">{user.username}</h3>
-          <div className="flex items-center">{inputs}</div>
+        <div className="flex  flex-row justify-stretch hover:bg-gray-50 py-4">
+          <p className="px-5 overflow-x-hidden text-ellipsis min-w-[10rem]">
+            {user.username}
+          </p>
+          <div className="flex flex-row space-x-4 position-center">
+            <p className="flex ">{inputs}</p>
+          </div>
         </div>
       )
     })
@@ -74,22 +81,29 @@ const PmTable = ({
     }
   }
 
+  const capitalizeFirstLetter = (name: string) => {
+    return name.charAt(0).toUpperCase() + name.slice(1)
+  }
+
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-row">
-        <div className="px-5">
+    <div className="flex flex-col w-full">
+      <div className="flex flex-row ">
+        <div className="px-5 overflow-x-hidden text-ellipsis min-w-[10rem] max-w-[10rem] space-y-2 my-4">
           <p>Month</p>
           <p>Budget</p>
         </div>
-        <div className="flex flex-row space-x-4">
+        <div className="flex flex-row ">
           {monthsAndBudget.map((monthEntry, index) => {
             const month = new Date(monthEntry.month)
+            const monthName = month.toLocaleString('default', { month: 'long' })
             return (
               <div
                 key={index}
-                className="flex flex-col space-x-4 items-center pl-14"
+                className="flex flex-col text-center space-y-2 min-w-[8rem] md:min-w-[10rem] border-x-2 my-4"
               >
-                <div>{month.toLocaleString('default', { month: 'long' })}</div>
+                <div className="center items-start">
+                  {capitalizeFirstLetter(monthName)}
+                </div>
                 <div> {monthEntry.amount} </div>
               </div>
             )
