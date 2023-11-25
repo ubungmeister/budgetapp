eec97bdd53418e5814380f12fd625fcf3f8b401b
ubungmeister <ubungmeister@gmail.com>
2023-11-25 19:54:08 +0100

    fix req goal

--- project/server/src/routes/savinggoal.ts
+++ project/server/src/routes/savinggoal.ts
@@ -125,14 +125,60 @@ router.post('/edit-goal', async (req, res) => {
 
 
 router.post('/delete-goal', async (req, res) => {
-  const {id, userId } = req.body
-    if(!userId) return res.status(400).json({ message: 'User id is required' });
-    if(!id) return res.status(400).json({ message: 'Goal id is required' });
-    await prisma.savingGoal.delete({
-        where: {id: id}
-    })
-    res.status(200).json({ message: 'Goal deleted successfully' });
-})
+  const { id, userId } = req.body;
 
+  if (!userId) {
+    return res.status(400).json({ message: 'User id is required' });
+  }
+
+  if (!id) {
+    return res.status(400).json({ message: 'Goal id is required' });
+  }
+
+  try {
+    // Start a transaction
+    const result = await prisma.$transaction(async (prisma) => {
+      const goal = await prisma.savingGoal.findUnique({
+        where: { id },
+      });
+
+      if (!goal) {
+        throw new Error('Goal not found');
+      }
+
+      const currentAmount = goal.currentAmount || 0;
+      if(currentAmount > 0 ){
+      // Create a new record for the refunded amount
+      await prisma.incomeOutcome.create({
+        data: {
+          amount: currentAmount,
+          category: 'Refund',
+          description: 'Refund from goal',
+          start_date: new Date(),
+          userId: userId,
+          category_type: 'Income',
+        },
+      });
+      }
+  
+      // Delete the goal
+      await prisma.savingGoal.delete({
+        where: { id },
+      });
+
+      return 'Goal deleted successfully';
+    });
+
+    res.status(200).json({ message: result });
+  } catch (error) {
+    // Check the error message to set the appropriate status code
+    if (error.message === 'Goal not found') {
+      res.status(404).json({ message: error.message });
+    } else {
+      console.error('Error deleting goal:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  }
+});
 
 export { router as savingGoalRouter }
